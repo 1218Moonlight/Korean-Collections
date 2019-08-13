5fe723c3328b616af045b1d84bd93aba6cb90d50
kioryu <1218moonlight@gmail.com>
2019-08-13 17:39:05 +0900

    ADD Tours Data Copyright

--- src/Component/Tour/Base.js
+++ src/Component/Tour/Base.js
@@ -2,9 +2,13 @@ import React from 'react'
 import ToolbarBase from '../Toolbar/Base'
 import AreaCode from '../Visitkorea/AreaCode'
 import MaterialTable from 'material-table';
+import Typography from '@material-ui/core/Typography'
+import {makeStyles} from '@material-ui/core/styles';
+import {styles} from '../../Resources/Styles/Tour'
 
 
-// Todo: Copyright
+const useStyles = makeStyles(styles);
+
 export default (function TourBase() {
     const [state] = React.useState({
         columns: [
@@ -29,30 +33,39 @@ export default (function TourBase() {
         // ],
     });
 
-
+    const classes = useStyles();
     return (
         <AreaCode render={({items, numOfRows, pageNo, totalCount}) => (
             <ToolbarBase>
                 {(() => {
                     // console.log(items, numOfRows, pageNo, totalCount);
                     const data = [];
-                    items.map(item => data.push({area: item.name._text, index: item.rnum._text, code: item.code._text}));
+                    items.map(item => data.push({
+                        area: item.name._text,
+                        index: item.rnum._text,
+                        code: item.code._text
+                    }));
 
                     if (numOfRows === '0') {
                         return (<p>Empty</p>) // Todo
                     } else {
                         // return (<p>Not Empty</p>)
-                        return <MaterialTable
-                            title="Area Tours"
-                            columns={state.columns}
-                            // data={state.data}
-                            data={data}
-                            actions={[{
-                                icon: 'save',
-                                tooltip: 'infomation',
-                                onClick: (event, rowData) => alert("Click " + rowData.area)
-                            }]}
-                        />
+                        return <div>
+                            <MaterialTable
+                                title="Area Tours"
+                                columns={state.columns}
+                                // data={state.data}
+                                data={data}
+                                actions={[{
+                                    icon: 'save',
+                                    tooltip: 'infomation',
+                                    onClick: (event, rowData) => alert("Click " + rowData.area)
+                                }]}
+                            />
+                            <Typography className={classes.Copyright} variant="overline" display="block" gutterBottom align={'center'}>
+                                영문 관광정보 서비스 by 한국관광공사, CC BY 라이센스
+                            </Typography>
+                        </div>
                     }
                 })()}
             </ToolbarBase>
