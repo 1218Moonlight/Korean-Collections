14325826385b2c6e43e7fa9714c2b1c63d184c2d
kioryu <1218moonlight@gmail.com>
2019-08-29 11:51:11 +0900

    ADD Tour [63 Building]

--- src/Resources/Contents/Tour.js
+++ src/Resources/Contents/Tour.js
@@ -18,6 +18,18 @@ import Gwanaksan3 from '../Img/tour/Gwanaksan Mountain (관악산)3.jpg'
 import Gwanaksan5 from '../Img/tour/Gwanaksan Mountain (관악산)5.jpg'
 import Gwanaksan6 from '../Img/tour/Gwanaksan Mountain (관악산)6.jpg'
 import Gwanaksan7 from '../Img/tour/Gwanaksan Mountain (관악산)7.jpg'
+import _63Art2 from '../Img/tour/63 Art (63 아트)2.jpg'
+import _63Art3 from '../Img/tour/63 Art (63 아트)3.jpg'
+import _63Art4 from '../Img/tour/63 Art (63 아트)4.jpg'
+import _63Art5 from '../Img/tour/63 Art (63 아트)5.jpg'
+import _63Square18 from '../Img/tour/63 Square (63 스퀘어)18.jpg'
+import _63Square19 from '../Img/tour/63 Square (63 스퀘어)19.jpg'
+import _63Square20 from '../Img/tour/63 Square (63 스퀘어)20.jpg'
+import _63Square21 from '../Img/tour/63 Square (63 스퀘어)21.jpg'
+import aquaPlanet2 from '../Img/tour/Aqua Planet 63 (아쿠아플라넷 63)2.jpg'
+import aquaPlanet3 from '../Img/tour/Aqua Planet 63 (아쿠아플라넷 63)3.jpg'
+import aquaPlanet4 from '../Img/tour/Aqua Planet 63 (아쿠아플라넷 63)4.jpg'
+import aquaPlanet5 from '../Img/tour/Aqua Planet 63 (아쿠아플라넷 63)5.jpg'
 
 const type = 3;
 
@@ -28,9 +40,17 @@ export const items = [
         title: "Seoulland", // (서울랜드)
         site: "http://eng.seoulland.co.kr",
         info: [
-            "Seoulland is Korea’s first theme park, with a beautiful scenery surrounded by Cheonggyesan Mountain. With more that 50 rides and other seasonal attractions, Seoulland offers thrill for people of all ages. Luna Land and Jurassic Land is not to be missed.",
-            "The pride of Seoulland is its wide selection of seasonal festivals and the unique Laser Show held during the evening. Seoul Grand Park is situated on a very large field and holds various festivals for each season of the year. The Tulip Festival is held from April to May; the Starlight Rose Festival from July to August; the Chrysanthemum Festival from September to October; and the Snow Light Festival from December to February.",
-            "Seoulland is a popular destination as it also has many attrations nearby such as the Seoul Grand Park, Forest Bath Resort, National Modern Art Center and more."
+            "Seoulland is Korea’s first theme park, with a beautiful scenery surrounded by Cheonggyesan Mountain. " +
+            "With more that 50 rides and other seasonal attractions, Seoulland offers thrill for people of all ages. " +
+            "Luna Land and Jurassic Land is not to be missed.",
+
+            "The pride of Seoulland is its wide selection of seasonal festivals and the unique Laser Show held during the evening. " +
+            "Seoul Grand Park is situated on a very large field and holds various festivals for each season of the year. " +
+            "The Tulip Festival is held from April to May; the Starlight Rose Festival from July to August; " +
+            "the Chrysanthemum Festival from September to October; and the Snow Light Festival from December to February.",
+
+            "Seoulland is a popular destination as it also has many attrations nearby such as the Seoul Grand Park, " +
+            "Forest Bath Resort, National Modern Art Center and more."
         ],
         imgs: [
             Seoulland2, Seoulland3, Seoulland4, Seoulland5
@@ -42,7 +62,10 @@ export const items = [
         title: "Seoulland Sledding Hills", // (서울랜드 눈썰매장)
         site: "http://eng.seoulland.co.kr",
         info: [
-            "Seoulland Sledding Hills offers two slopes, one for children (50 m long) and another for adults (120 m long). Amenities include food stands that sell various foods, including eomuk (fish cake) soup, resting lounge and medical office. Various performances are also prepared to entertain visitors at the main stage nearby the sledding hill.",
+            "Seoulland Sledding Hills offers two slopes, one for children (50 m long) and another for adults (120 m long). " +
+            "Amenities include food stands that sell various foods, including eomuk (fish cake) soup, " +
+            "resting lounge and medical office. " +
+            "Various performances are also prepared to entertain visitors at the main stage nearby the sledding hill.",
         ],
         imgs: [
             Sledding2, Sledding3, Sledding4, Sledding5
@@ -54,9 +77,19 @@ export const items = [
         title: "Incheon International Airport", // (인천국제공항)
         site: "https://www.airport.kr/ap/en/index.do",
         info: [
-            "Incheon International Airport is one of the main transport hubs of Northeast Asia. Opened in March 2001, it was built on reclaimed land between Yeongjongdo and Yongyudo Islands. It houses the Korean Culture Museum, Traditional Culture Experience Zone, Traditional Craft Gallery, and Arrival Hall Culture Street, providing domestic and international tourists with opportunities to appreciate the richness of Korean arts and culture.",
-            "Airstar Terrace is an elegant lounge, overlooking aircrafts from around the world. The Incheon International Airport has continued to win awards ever since its opening and has been praised for its excellence, down to the last detail, some even commenting on the beauty of the lights at night reflecting on the aircrafts moored in the area.",
-            "The airport is consistantly ranked in the top ten airports worldwide and was granted the title ‘Best Airport in the World’ by Global Traveler for four consecutive years. It is the second busiest airport in the world in terms of cargo and freight and one of the busiest airports in terms of international passengers."
+            "Incheon International Airport is one of the main transport hubs of Northeast Asia. " +
+            "Opened in March 2001, it was built on reclaimed land between Yeongjongdo and Yongyudo Islands. " +
+            "It houses the Korean Culture Museum, Traditional Culture Experience Zone, Traditional Craft Gallery, " +
+            "and Arrival Hall Culture Street, " +
+            "providing domestic and international tourists with opportunities to appreciate the richness of Korean arts and culture.",
+
+            "Airstar Terrace is an elegant lounge, overlooking aircrafts from around the world. " +
+            "The Incheon International Airport has continued to win awards ever since its opening and has been praised for its excellence, " +
+            "down to the last detail, some even commenting on the beauty of the lights at night reflecting on the aircrafts moored in the area.",
+
+            "The airport is consistantly ranked in the top ten airports worldwide and was granted the title " +
+            "‘Best Airport in the World’ by Global Traveler for four consecutive years. " +
+            "It is the second busiest airport in the world in terms of cargo and freight and one of the busiest airports in terms of international passengers."
         ],
         imgs: [
             incheonAirport2, incheonAirport3, incheonAirport4, incheonAirport5
@@ -68,7 +101,13 @@ export const items = [
         title: "Rail Cruise Haerang", // (레일크루즈 해랑열차)
         site: "http://www.railcruise.co.kr/en/infault.asp",
         info: [
-            "Rail Cruise Haerang, a hotel-on-wheels, began operating in November 2008 as the nation's first tourist train with sleeping accommodations. With a name meaning \"together with the sun,\" Haerang is designed to be a luxury train similar to a yacht traveling along a beautiful mountain river beneath the bright sun. The exterior of the train is painted in a deep blue with gold lines and a logo featuring a phoenix while the interior design has Korean elements. Tourists will enjoy their time on the train, with personalized trips and full amenities.",
+            "Rail Cruise Haerang, a hotel-on-wheels, " +
+            "began operating in November 2008 as the nation's first tourist train with sleeping accommodations. " +
+            "With a name meaning \"together with the sun,\" Haerang is designed to be a luxury train similar to a yacht " +
+            "traveling along a beautiful mountain river beneath the bright sun. " +
+            "The exterior of the train is painted in a deep blue with gold lines and a logo featuring a phoenix while " +
+            "the interior design has Korean elements. " +
+            "Tourists will enjoy their time on the train, with personalized trips and full amenities.",
         ],
         imgs: [
             RailHaerang2, RailHaerang3, RailHaerang4, RailHaerang5
@@ -80,11 +119,69 @@ export const items = [
         title: "Gwanaksan Mountain", // (관악산)
         site: "http://www.gwanak.go.kr/site/eng/main.do",
         info: [
-            "At 632.2 meters in x_height, Gwanaksan Mountain is the symbol and pride of Gwanak-gu district in Seoul. Most of the cultural heritages of the district originate from Gwanaksan Mountain. Since it was designated as a city natural park in 1968, it has continued to serve as a favorite place for relaxation and excursion for Seoul citizens. The various rocky peaks and the deep valleys give the mountain a rugged feel. The mountain's size and close proximity to Seoul make it easy for Seoul residents to visit in a single day.",
-            "In the spring, cherry blossoms are in full bloom near the entrance to the mountain, and a Rhododendron Festival is held when the rhododendrons are in full bloom. At the mountain's summit are Wongaksa Temple and Yeonjuam Hermitage, which were built by Taejo Yi Seong-gye (the founder of the Joseon dynasty) to ward off misfortune when he decided to move the capital to Seoul. There are also other temples and hermitages, and a ground radar observation post. Yeonjudae Hermitage, located atop a cliff, is where all the hiking trails of Gwanaksan Mountain meet."
+            "At 632.2 meters in x_height, Gwanaksan Mountain is the symbol and pride of Gwanak-gu district in Seoul. " +
+            "Most of the cultural heritages of the district originate from Gwanaksan Mountain. Since it was designated " +
+            "as a city natural park in 1968, it has continued to serve as a favorite place for relaxation and excursion " +
+            "for Seoul citizens. The various rocky peaks and the deep valleys give the mountain a rugged feel. The mountain's " +
+            "size and close proximity to Seoul make it easy for Seoul residents to visit in a single day.",
+
+            "In the spring, cherry blossoms are in full bloom near the entrance to the mountain, and a Rhododendron " +
+            "Festival is held when the rhododendrons are in full bloom. At the mountain's summit are Wongaksa Temple " +
+            "and Yeonjuam Hermitage, which were built by Taejo Yi Seong-gye (the founder of the Joseon dynasty) to " +
+            "ward off misfortune when he decided to move the capital to Seoul. There are also other temples and hermitages, " +
+            "and a ground radar observation post. Yeonjudae Hermitage, located atop a cliff, is where all the hiking " +
+            "trails of Gwanaksan Mountain meet."
         ],
         imgs: [
             Gwanaksan3, Gwanaksan5, Gwanaksan6, Gwanaksan7
+        ]
+    },
+    {
+        // update : 2019-08-29
+        type: type,
+        title: "63 Art", // (63 아트)
+        site: "www.63art.co.kr",
+        info: [
+            "63 City in Yeouido stands 264 meters above sea level and is considered one of Seoul’s most well known landmarks. " +
+            "The 60th floor ‘Sky Deck’ of 63 City Building was remodeled into an art museum, offering a place for art exhibitions, " +
+            "magic shows, and various cultural experiences. Visitors will also be able to enjoy the beautiful scenery of Hangang " +
+            "River through the observatory’s large windows.",
+
+            "The Art Museum is located on the 60th floor and ticket must be purchased to visit the museum from 63 City ticket booth."
+        ],
+        imgs: [
+            _63Art2, _63Art3, _63Art4, _63Art5
+        ]
+    },
+    {
+        // update : 2019-08-29
+        type: type,
+        title: "63 Square", // (63 스퀘어)
+        site: "http://www.63.co.kr/en/index.do",
+        info: [
+            "With 63 floors measuring a total x_height of 249 meters, 63 Building is one of Korea’s tallest and most " +
+            "recognized building. 63 Building boasts spectacular views of the Hangang River and the surrounding Bugaksan, " +
+            "Namsan and Gwanaksan Mountains. The building also offers an outstanding vantage point when viewing the sunset " +
+            "over the city."
+        ],
+        imgs: [
+            _63Square18, _63Square19, _63Square20, _63Square21
+        ]
+    },
+    {
+        // update : 2019-08-29
+        type: type,
+        title: "Aqua Planet 63", // (아쿠아플라넷 63)
+        site: "http://www.aquaplanet.co.kr/63/eng/index.jsp",
+        info: [
+            "Aqua Planet 63 located in 63 Square was opened July 1, 2016. The aquarium houses Otter Planet; Aqua Garden " +
+            "featuring waterplants and Amazonian plants and lungfish; " +
+            "Golden Miracle Zone decorated with bright sunflowers; Penguin Planet; Seal Planet; Coral Planet, colorful " +
+            "with various underwater coral plants and more zones for an " +
+            "exciting space. In the main tank, visitors can watch the fantastic mermaid show, set to music and lights."
+        ],
+        imgs: [
+            aquaPlanet2, aquaPlanet3, aquaPlanet4, aquaPlanet5
         ]
     },
 ];
