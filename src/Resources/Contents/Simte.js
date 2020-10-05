0da86a93b058611bba09ee9076acb145851965c0
kioryu <1218moonlight@gmail.com>
2020-10-05 23:05:43 +0900

    [ Simte ] 1. 오타 수정 : 방갑습니다. -> 반갑습니다. 2. 이미지 수정 (A.jpg, B.jpg 추가)

--- src/Resources/Contents/Simte.js
+++ src/Resources/Contents/Simte.js
@@ -4,7 +4,7 @@ export const step = [
         ["Hello", "안녕하세요."],
         ["Ka min A ahi", "제 이름은 A 입니다."],
         ["Ka min B ahi", "제 이름은 B 입니다."],
-        ["I'm glad to meet you", "만나서 방갑습니다."],
+        ["I'm glad to meet you", "만나서 반갑습니다."],
         ["So do I", "저도요."]
     ],
     [
